2189856d42069eef5a18d94474cdd6091da06f48
AtharvaKalsekar <kalsekar.atharva7@gmail.com>
2021-02-21 22:23:47 +0530

    added bootstrap and react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+      </div>
+  </Router>,
   document.getElementById('root')
 );
 
